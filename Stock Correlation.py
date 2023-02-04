e9ec038c1a055036fbf8fbc9ef7118c90270d6f0
Nick Stambaugh <73977662+NicholasStambaugh@users.noreply.github.com>
2023-02-04 14:25:30 -0500

    Add files via upload

---
+++ Stock Correlation.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+
+etfs = ['voo', 'ms', 'inds', 'xlsr', 'iyr', 'spy', 'qqq', 'msft', 'vt', 'sdy', 'cqqq', 'soxl', 'spem', 'googl', 'wmt', 'ivv', 'vxus', 'agg', 'schd']
+
+df = pd.DataFrame()
+
+today = datetime.now().date().strftime("%Y-%m-%d")
+
+for etf in etfs:
+    df[etf] = yf.Ticker(etf).history(start="2010-09-08", end=today).Close
+
+df = df.diff()
+print(df.diff())
+
+df = df.corr()
+print(df.corr())
+
+
+
+
